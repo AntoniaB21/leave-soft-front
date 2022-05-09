2d073e42ec0ea07af8e9d9334ee63341a033e138
= <antonia.balluais@mail-ecv.fr>
2022-05-09 04:28:02 +0200

    remove unused slice

--- src/types/RootState.ts
+++ src/types/RootState.ts
@@ -5,7 +5,6 @@ import { LoginUserPageState } from 'app/pages/LoginUserPage/slice/types';
 import { ProfilePageState } from 'app/pages/ProfilePage/slice/types';
 import { OffRequestAddState } from 'app/pages/OffRequestAdd/slice/types';
 import { MyOffsListState } from 'app/pages/MyOffsList/slice/types';
-import { NotificationsPageState } from 'app/pages/NotificationsPage/slice/types';
 import { UsersListPageState } from 'app/pages/UsersListPage/slice/types';
 import { AddUserPageState } from 'app/pages/AddUserPage/slice/types';
 // [IMPORT NEW CONTAINERSTATE ABOVE] < Needed for generating containers seamlessly
@@ -21,7 +20,6 @@ export interface RootState {
   profilePage?: ProfilePageState;
   offRequestAdd?: OffRequestAddState;
   myOffsList?: MyOffsListState;
-  notificationsPage?: NotificationsPageState;
   usersListPage?: UsersListPageState;
   addUserPage?: AddUserPageState;
   // [INSERT NEW REDUCER KEY ABOVE] < Needed for generating containers seamlessly
